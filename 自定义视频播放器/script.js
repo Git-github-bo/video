26ff527654189f8bf691d4512ac3f1073729574c
Songwenbo <git_wenbo@163.com>
2021-07-03 13:21:46 +0800

    2021/7/3

--- 自定义视频播放器/script.js
+++ 自定义视频播放器/script.js
@@ -1,43 +1,58 @@
+// 获取HTML
 const video = document.getElementById('video');
 const play = document.getElementById('play');
 const stop = document.getElementById('stop');
 const progress = document.getElementById('progress');
 const timestamp = document.getElementById('timestamp');
 
+// 切换视频的播放/暂停
+// paused 视频已暂停属性，用于返回视频是否已暂停，它是只读属性。
+// video.paused 如果视频已暂停返回布尔值true,否则返回false
 function toggleVideoStatus() {
     if (video.paused) {
         video.play();
-    }else {
+    } else {
         video.pause();
     }
 }
 
+// 切换按钮的播放/暂停
 function updatePlayIcon() {
     if (video.paused) {
         play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
-    }else {
+    } else {
         play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
     }
 }
+
+// input 进度条
 function updateProgress() {
     progress.value = (video.currentTime / video.duration) * 100;
     let mins = Math.floor(video.currentTime / 60);
-    if(mins < video.duration) {
+    if (mins < video.duration) {
         mins = '0' + String(mins);
     }
     let secs = Math.floor(video.currentTime % 60);
-    if(mins < video.duration){
+    if (mins < video.duration) {
         secs = '0' + String(secs);
     }
     timestamp.innerHTML = `${mins}:${secs}`;
 }
+
+// 滑动进度条调整到当前进度
 function setVideoProgress() {
     video.currentTime = (+progress.value * video.duration) / 100;
 }
+
+// 点击按钮中止并暂停
 function stopVideo() {
     video.currentTime = 0;
+
+
     video.pause();
 }
+
+// 添加事件句柄
 video.addEventListener('click', toggleVideoStatus);
 video.addEventListener('pause', updatePlayIcon);
 video.addEventListener('play', updatePlayIcon);
